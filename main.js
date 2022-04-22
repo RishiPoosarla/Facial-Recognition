d39b60f298f65e1ea06d25d93bffe31c0be47e7b
RishiPoosarla <88695245+RishiPoosarla@users.noreply.github.com>
2022-04-21 18:37:53 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -7,13 +7,27 @@ png_quality: 90
 });
 camera = document.getElementById("camera");
 Webcam.attach("#camera");
-function take_snapshot(){
+function takeSnapshot(){
     Webcam.snap(function(data_uri){
         document.getElementById("result").innerHTML = '<img id="captured_image" src="'+ data_uri +'">';
     });
 }
 console.log("ml5_version-" + ml5.version);
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/zMvu4d8QM/", modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/zMvu4d8QM/model.json", modelLoaded);
 function modelLoaded(){
     console.log("model is loaded");
+}
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img, got_result);
+}
+function got_result(error, results){
+if (error){
+    console.log(error);
+}
+else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+}
 }
